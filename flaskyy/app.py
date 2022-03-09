e47968e76ac0a1a07f39485c798b62ed9df95fe1
Conor McCaulley <conormccaulley@gmail.com>
2022-03-08 17:07:25 -0800

    integrated ml  gen model into flask app

--- flaskyy/app.py
+++ flaskyy/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, g, current_app
 import sqlite3
 import pandas as pd
 import random
+import numpy as np
+import pickle
+import tensorflow as tf
+from scipy.special import softmax
 
 app = Flask(__name__)
 
@@ -83,10 +87,71 @@ def finder():
                                category = category)
 @app.route('/generator/', methods=['POST', 'GET'])
 def generator():
+    #load in our saved model
+    model=tf.keras.models.load_model('recipe_gen_model.h5')
+
+    #load in our chars variable from the chars.pkl file
+    chars= pickle.load(open('chars.pkl', 'rb'))
+
+    X=pickle.load(open('X.pkl', 'rb'))
+
+    lines=pickle.load(open('lines.pkl', 'rb'))
+
+
     if request.method == 'GET':
         return render_template('generator.html')
     else:
-        n = request.form['n']
-        newn = generate(n)
-        return render_template('generator.html', n=newn, word=n)
+        gen_legth = request.form['gen_legth']
 
+        new_recipe=generate_string(gen_legth, model,X,chars,lines)
+
+        return render_template('generator.html', new_recipe=new_recipe, legth= gen_legth)
+
+
+
+def generate_string(gen_length, model, X, chars,lines): 
+    temp=.1
+    seed_index = random.randint(0, 50)
+    max_len=20
+    X=X
+
+    # sequence of integer indices for generated text
+    gen_seq = np.zeros((max_len + gen_length, 110))
+    
+    # first part of the generated indices actually corresponds to the real text
+    seed = X[seed_index]
+    gen_seq[0:max_len] = seed
+    
+    # character version
+    gen_text = lines[seed_index]
+ 
+    for i in range(0, gen_length):
+  
+        window = gen_seq[i: i + max_len]
+
+        preds = model.predict(np.array([window]))[0]
+        preds = softmax(preds)
+        next_index = sample(preds, temp)
+        
+        # add sampled index to the current output
+        gen_seq[max_len + i, next_index] = True
+        
+        # create the string version
+        next_char = chars[next_index]
+        gen_text += next_char
+    
+    # only return the string version because that's what we care about
+    return(gen_text)
+
+
+    def sample(preds, temp):
+        # format the model predictions
+        preds = np.asarray(preds).astype("float64")
+        
+        # construct normalized Boltzman with temp
+        probs = np.exp(preds/temp)
+        probs = probs / probs.sum()
+        
+        # sample from Boltzman
+        samp = np.random.multinomial(1, probs, 1)
+        return np.argmax(samp)
